6d818085b0c411896799123ca7bb1a75aab82dbd
Nicole Dow <nikki.dow71@gmail.com>
2019-06-12 21:44:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,9 @@ def load_library(file)
   end
   
   emoticon_library.each do |emoji_title, faces_array|
-    dictionary["get_emoticon"][emoji_title] = faces_array
+    faces_array.each do |face|
+      binding.pry
+    end
   end
   
   dictionary
